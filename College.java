3da5dbcd83e7429dd31a0fc73c4f94da0e762a90
KalaivaniShankar <89274886+KalaivaniShankar@users.noreply.github.com>
2021-08-21 00:44:01 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,37 @@
+package org.college;
+
+public class College {
+
+
+public void collegeName() {
+	String jayacollege; 
+	
+	System.out.println("college name=jayacollege");
+}
+public void collegeCode() {
+	int code;
+	System.out.println("college code=22222");
+}
+public void collegeRank() {
+	int rank;
+	System.out.println("college rank=2");
+}
+
+	public static void main(String[] args) {
+		College cb=new College();
+		cb.collegeName();
+		cb.collegeCode();
+		cb.collegeRank();
+Student rv=new Student();
+rv.studentName();
+rv.studentDepartment();
+rv.studentId();
+Hostel hh=new Hostel();
+hh.hostelName();
+Dept dd
+=new Dept();
+dd.departmentName();
+
+	}
+
+}
